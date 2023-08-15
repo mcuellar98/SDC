cf5766103c97d94e4c86af1eb6d0a0de235db7af
Amelia LI <jkramelia8872@gmail.com>
2023-08-14 23:46:06 -0400

    add fields for the review form

--- client/src/components/componentA/Modal.jsx
+++ client/src/components/componentA/Modal.jsx
@@ -5,17 +5,25 @@ import img1 from '../resources/reviewmail.svg';
 const Modal = ({openModal, setOpenModal}) => {
 
   const[recommend, setRecommend] = useState(null);
+  const [reviewSummary, setReviewSummary] = useState('');
+  const [reviewBody, setReviewBody] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
 
   const handleRecommendChange = (value) => {
     setRecommend(value);
-  }
+  };
+
+  const handleReviewSummaryChange = (event) => {
+    setReviewSummary(event.target.value);
+  };
 
   return (
     <>
     <div className="main-container">
       <div className="modal-container">
         <img className="model-image w-24 h-24" src={img1} />
-        <h2>Write Your Review</h2>
+        <h1 className="font-bold">Write Your Review</h1>
       </div>
       <div className="reviewForm">
         <div>
@@ -43,12 +51,61 @@ const Modal = ({openModal, setOpenModal}) => {
             />
             No
           </label>
-
         </div>
+
+        <div>
+          <label>Review Summary</label>
+          <input
+          type="text"
+          value={reviewSummary}
+          onChange={handleReviewSummaryChange}
+          placeholder="Example: Best purchase ever!"
+          maxLength="60"
+          />
+        </div>
+
         <div>
           <label>Review body:</label>
-          <input type="text" />
+          <textarea
+          value={reviewBody}
+          onChange={handleReviewBodyChange}
+          placeholder="Why did you like the product or not?"
+          maxLength="1000"
+           />
+           <div>
+            {counterMessage}
+           </div>
         </div>
+
+        <div>
+          {/* upload images */}
+        </div>
+
+        <div>
+          <label>What is your nickname?</label>
+          <input
+            type="text"
+            value={nickname}
+            onChange={handleNicknameChange}
+            placeholder="Example: jackson11!"
+            maxLength="60"
+          />
+          <div>For privacy reasons, do not use your full name or email address</div>
+        </div>
+
+        <div>
+          <label>Your email </label>
+          <input
+            type="text"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Example: jackson11@email.com"
+            maxLength="60"
+          />
+          <div>For authentication reasons, you will not be emailed</div>
+
+        </div>
+
         <div>
           <button className="modal-button-send">Submit</button>
           <br></br>
