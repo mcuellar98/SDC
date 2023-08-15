6473beedd16d40d23068ae6a7b5bd0a221c2b8c6
Tonegawa94 <victor1394@gmail.com>
2023-08-15 15:13:21 -0500

    displaying questions based on helpfulness

--- client/src/components/componentV/Q&A.jsx
+++ client/src/components/componentV/Q&A.jsx
@@ -5,21 +5,36 @@ import IdividualAnswer from "./QnAcomponents/indvAnswer.jsx";
 import MoreAnswers from "./QnAcomponents/moreAnswers.jsx";
 import AddQuestions from "./QnAcomponents/addQuestion.jsx";
 const QuestionsAndAnswers = () => {
-const product = '37323'
-
+  const product = '37323'
 
 useEffect(() => {
   axios.get(`/questions/${product}`).then((result) => {
     updateQuestions(result.data.results)
+    wasUpdated(true)
   }).catch((err) => {
     console.error(err)
   })
 }, [])
-
+const [updated, wasUpdated] = useState(false)
 const [questions, updateQuestions] = useState([{
   question_body: 'Please wait for page to load',
   question_date: '2020-11-16T00:00:00.000Z',
 }])
+
+
+useEffect(() => {
+  console.log(questions)
+  questions.sort((a,b) => {
+    return b.question_helpfulness
+    - a.question_helpfulness
+
+  })
+  var topQuestions = questions.slice(0,4)
+updateQuestions([...topQuestions])
+// updateTwo([...twoAnswers])
+},[updated])
+
+
 
 return (
   <div className="pl-24 pt-2 bg-neutral-800 text-white ">
@@ -36,7 +51,7 @@ return (
 
     </div>
     <div className="my-1.5">
-    <MoreAnswers /> <AddQuestions />
+    <button className="rounded bg-neutral-400	 p-3">More Answered Questions</button> <AddQuestions />
     </div>
   </div>
 )
