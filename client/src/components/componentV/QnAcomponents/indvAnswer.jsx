1f042b0d43f499aceb4be4e3f9f801826b44d199
Tonegawa94 <victor1394@gmail.com>
2023-08-16 16:22:23 -0500

    load more answers changes to collapse answers when showing all answers

--- client/src/components/componentV/QnAcomponents/indvAnswer.jsx
+++ client/src/components/componentV/QnAcomponents/indvAnswer.jsx
@@ -4,29 +4,38 @@ import Helpful from "./Helpful.jsx"
 import AnswerModal from "./modal/answermodal.jsx"
 const IdividualAnswer = (props) => {
 
-  console.log(props)
+  const [collapse, updateCollapse] = useState(props.answers.length > 2)
   var loopAnswers = props.answers
-
   useEffect(() => {
     loopAnswers.sort((a, b) => {
       return b.helpfulness - a.helpfulness
     })
+    if (loopAnswers.length < 2) {
+      updateCollapse(false)
+    }
     var twoAnswers = loopAnswers.slice(0, 2)
     updateAnswers([...twoAnswers])
     updateTwo([...twoAnswers])
   }, [props.answers, answers])
+
   const [showModal, updateShow] = useState(false)
   const [two, updateTwo] = useState(0);
   const [answers, updateAnswers] = useState(props.answers)
 
-  // <p className="hover:text-stone-50"> Add Answer </p>
+const handleLoad = () => {
+    if (answers.length <= 2) {
+      updateAnswers(loopAnswers)
+    } else {
+      updateAnswers(two)
+      updateCollapse(false)
+    }
+}
   return (
     <div>
       <div className="flex w-full justify-between ">
         <div className="text-xl">
           Q: {props.questions.question_body}
-          {showModal && <AnswerModal updateShow={updateShow} questionID={props.questions.question_id}/>}
-
+          {showModal && <AnswerModal updateShow={updateShow} questionID={props.questions.question}/>}
         </div> <span className="flex text-xs text-stone-400 pr-2.5" ><Helpful />&nbsp;| &nbsp;<p className="hover:text-stone-50" onClick={(e) => {
           updateShow(!showModal)
         }}> {showModal && 'Cancel'} {!showModal && 'Add Answer'}</p></span>
@@ -38,13 +47,7 @@ const IdividualAnswer = (props) => {
               return <AnswerItem answer={answer} key={index} />
             })}
           </div>
-          {props.answers.length > 2 && <p onClick={(e) => {
-            if (answers.length <= 2) {
-              updateAnswers(loopAnswers)
-            } else {
-              updateAnswers(two)
-            }
-          }}>Load More Answers</p>}
+          {props.answers.length > 2 && <p onClick={(e)=> handleLoad()}>{answers.length <= 2 ? 'Load More Answers' : "Collapse Answers"}</p>}
         </div>
       </div>
     </div>
--- client/src/components/componentV/QnAcomponents/modal/answermodal.jsx
+++ client/src/components/componentV/QnAcomponents/modal/answermodal.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import axios from "axios";
+
 const AnswerModal = (props) => {
 
 return (
