a2793f55ab59f3a24336c4d21faf1524c92ebed8
Tonegawa94 <victor1394@gmail.com>
2023-08-16 14:06:45 -0500

    added answer modal with functionality does not post anything yet

--- client/src/components/componentV/QnAcomponents/indvAnswer.jsx
+++ client/src/components/componentV/QnAcomponents/indvAnswer.jsx
@@ -1,47 +1,53 @@
-import React, {useState, useEffect}  from "react";
-import Answer from "./answer.jsx"
+import React, { useState, useEffect } from "react";
 import AnswerItem from "./answerList.jsx"
 import Helpful from "./Helpful.jsx"
-
+import AnswerModal from "./modal/answermodal.jsx"
 const IdividualAnswer = (props) => {
+
+  console.log(props)
   var loopAnswers = props.answers
 
   useEffect(() => {
-    loopAnswers.sort((a,b) => {
+    loopAnswers.sort((a, b) => {
       return b.helpfulness - a.helpfulness
     })
-    var twoAnswers = loopAnswers.slice(0,2)
-  updateAnswers([...twoAnswers])
-  updateTwo([...twoAnswers])
-  },[props.answers, answers])
+    var twoAnswers = loopAnswers.slice(0, 2)
+    updateAnswers([...twoAnswers])
+    updateTwo([...twoAnswers])
+  }, [props.answers, answers])
+  const [showModal, updateShow] = useState(false)
   const [two, updateTwo] = useState(0);
   const [answers, updateAnswers] = useState(props.answers)
 
+  // <p className="hover:text-stone-50"> Add Answer </p>
   return (
     <div>
+      <div className="flex w-full justify-between ">
+        <div className="text-xl">
+          Q: {props.questions.question_body}
+          {showModal && <AnswerModal updateShow={updateShow} questionID={props.questions.question_id}/>}
 
-    <div className="flex w-full justify-between ">
-      <p className="text-xl">
-        Q: {props.questions.question_body}
-      </p> <span className="flex text-xs text-stone-400"><Helpful />&nbsp;| &nbsp;<Answer /></span>
+        </div> <span className="flex text-xs text-stone-400 pr-2.5" ><Helpful />&nbsp;| &nbsp;<p className="hover:text-stone-50" onClick={(e) => {
+          updateShow(!showModal)
+        }}> {showModal && 'Cancel'} {!showModal && 'Add Answer'}</p></span>
       </div>
-    <div className="indAnswer">
-      <div className="outer" >
-      <p className="text-xl max-w-xl">
-        {answers.map((answer, index) => {
-          return  <AnswerItem answer={answer} key={index}/>
-        })}
-      </p>
-      {props.answers.length > 2 && <p onClick={ (e) => {
-        if (answers.length <= 2) {
-          updateAnswers(loopAnswers)
-        } else {
-          updateAnswers(two)
-        }
-      }}>Load More Answers</p>}
+      <div className="indAnswer">
+        <div className="outer" >
+          <div className="text-xl max-w-xl">
+            {answers.map((answer, index) => {
+              return <AnswerItem answer={answer} key={index} />
+            })}
+          </div>
+          {props.answers.length > 2 && <p onClick={(e) => {
+            if (answers.length <= 2) {
+              updateAnswers(loopAnswers)
+            } else {
+              updateAnswers(two)
+            }
+          }}>Load More Answers</p>}
+        </div>
       </div>
     </div>
-      </div>
   )
 }
 
---
+++ client/src/components/componentV/QnAcomponents/modal/answermodal.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import axios from "axios";
+const AnswerModal = (props) => {
+
+return (
+    <div className="absolute right-0">
+      <form className="grid grid-col-2 gap-2 mr-4 bg-neutral-200 p-2" onSubmit={(e) => {
+        e.preventDefault()
+        console.log(e.target[0].value)
+      }} >
+      <textarea
+        id={'answerTextAreaID'}
+        name={"answer conent"}
+        placeholder="Add Answer Here"
+        rows={4}
+        cols={40}
+        className="col-span-2 text-base rounded-sm text-black"
+      />
+        <input placeholder="Nickname"className="rounded-sm text-base p-1 text-black"/>
+        <input placeholder="Email" className="rounded-sm text-base p-1 text-black"/>
+        <input type="button" value="Submit Photo" className="rounded bg-neutral-500	 p-1"/>
+        <input type="Submit" value="Submit" className="rounded bg-neutral-500	 p-1" readOnly={true}/>
+      </form>
+    </div>
+)
+}
+
+export default AnswerModal;
