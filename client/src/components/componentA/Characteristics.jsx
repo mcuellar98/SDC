7f7480129d3a381658ee127ce78c28929650a0a7
Amelia LI <jkramelia8872@gmail.com>
2023-08-15 23:50:35 -0400

    add 6 characteristics and the meaning, radiobox

---
+++ client/src/components/componentA/Characteristics.jsx
@@ -0,0 +1,118 @@
+import React, {useState} from 'react';
+
+const Characteristics = () => {
+
+  const characteristicsData = [
+    {
+      title: 'Size',
+      meanings: [
+      'A size too small',
+      '½ a size too small',
+      'Perfect',
+      '½ a size too big',
+      'A size too wide',
+    ],
+  },
+  {
+    title: 'Width',
+    meanings: [
+    'Too narrow',
+    'Slightly narrow',
+    'Perfect',
+    'Slightly wide',
+    'Too wide',
+    ],
+  },
+  {
+    title: 'Comfort',
+    meanings: [
+    'Uncomfortable',
+    'Slightly uncomfortable',
+    'Ok',
+    'Comfortable',
+    'Perfect',
+    ],
+  },
+  {
+    title: 'Quality',
+    meanings: [
+    'Poor',
+    'Below average',
+    'What I expected',
+    'Pretty great',
+    'Perfect',
+    ],
+  },
+  {
+    title: 'Length',
+    meanings: [
+    'Runs Short',
+    'Runs slightly short',
+    'Perfect',
+    'Runs slightly long',
+    'Runs long',
+    ],
+  },
+  {
+    title: 'Fit',
+    meanings: [
+    'Runs tight',
+    'Runs slightly tight',
+    'Perfect',
+    'Runs slightly long',
+    'Runs long',
+    ],
+  },
+
+]
+
+const [characteristicsRatings, setCharacteristicsRatings] = useState(
+  characteristicsData.map(() => null)
+);
+
+const handleCharacteristicRatingChange = (index, value) => {
+  const updatedRatings = [...characteristicsRatings];
+  updatedRatings[index] = value;
+  setCharacteristicsRatings(updatedRatings);
+};
+
+return (
+  <div className="characteristics">
+    <h2 className="font-bold text-lg mb-2">Characteristics</h2>
+    <div className="flex flex-col space-y-4">
+      {characteristicsData.map((characteristic, index) => (
+        <div key={index} className="flex items-center">
+          <label className="font-normal mr-2">{characteristic.title}</label>
+          {Array.from({ length: 5 }, (_, rating) => rating + 1).map(
+            (rating) => (
+              <label
+                key={rating}
+                className={`flex items-center cursor-pointer mr-4 ${
+                  characteristicsRatings[index] === rating
+                    ? 'text-purple-600'
+                    : 'text-gray-600'
+                }`}
+              >
+                <input
+                  type="radio"
+                  name={`characteristic_${index}`}
+                  value={rating}
+                  onChange={() =>
+                    handleCharacteristicRatingChange(index, rating)
+                  }
+                  className="mr-1"
+                />
+                {characteristic.meanings[rating - 1]}
+              </label>
+            )
+          )}
+        </div>
+      ))}
+    </div>
+  </div>
+);
+};
+
+
+
+export default Characteristics;
