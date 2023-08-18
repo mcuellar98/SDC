d4ccf758f8c75e996c874546990131618849b7d5
Amelia LI <jkramelia8872@gmail.com>
2023-08-18 10:07:40 -0400

    add image modal

--- client/src/components/componentA/ReviewEntry.jsx
+++ client/src/components/componentA/ReviewEntry.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Star from './Star.jsx'
+import React, {useState} from 'react';
+import Star from './Star.jsx';
+import ImageModal from './ImageModal.jsx';
 
 const ReviewEntry = ({ review }) => {
   const dateObj = new Date(review.date);
@@ -7,10 +8,17 @@ const ReviewEntry = ({ review }) => {
   const formattedDate = dateObj.toLocaleDateString('en-US', options);
 
   const [showFullReview, setShowFullReview] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [openImageModal, setOpenImageModal] = useState(false);
 
   const handleShowMore = () => {
     setShowFullReview(true);
   }
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+    setOpenImageModal(true);
+  };
 
   return (
     <div className="relative box-border h-50 w-50 p-4 border-4 reviewTile" key={review.review_id}>
@@ -19,6 +27,8 @@ const ReviewEntry = ({ review }) => {
       </div>
       <Star rating={review.rating} />
 
+      <div className="reviewSummary font-semibold">{review.summary}</div>
+
       <div className={`reviewBody font-normal ${showFullReview ? 'expanded' : ''}`}>
         {showFullReview ? review.body : review.body.substring(0, 250)}
         {!showFullReview && review.body.length > 250 && (
@@ -26,12 +36,29 @@ const ReviewEntry = ({ review }) => {
         )}
       </div>
 
-      <div className="reviewSummary font-semibold">{review.summary}</div>
-      <div className="reviewBody font-normal">{review.body}</div>
-      <div>{review.recommend ? 'I recommend this product' : null}</div>
-      <div>Helpful? Yes({review.helpfulness}) </div>
+      {review.photos && review.photos.length > 0 && (
+        <div className="imageThumbnail w-1/5 flex">
+          {review.photos.map((image, index) => (
+            <img
+            key ={index}
+            src={image.url}
+            onClick={() => handleImageClick(image)}
+            />
+          ))}
+        </div>
+      )}
 
-      {/* Render other review details as needed */}
+      {openImageModal && (
+        <ImageModal
+        imageUrl={selectedImage.url}
+        closeModal={() => setOpenImageModal(false)}
+        />
+      )}
+
+
+      {review.recommend && <div className="recommendation">I recommend this product</div>}
+      <div>Helpful? Yes ({review.helpfulness})</div>
+
     </div>
   );
 }
