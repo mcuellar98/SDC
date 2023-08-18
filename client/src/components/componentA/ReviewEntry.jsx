702264342e1bffabada6da8471c48bf0729fe086
Amelia LI <jkramelia8872@gmail.com>
2023-08-18 09:22:48 -0400

    handle show more button in review body length greathen than 250

--- client/src/components/componentA/ReviewEntry.jsx
+++ client/src/components/componentA/ReviewEntry.jsx
@@ -6,12 +6,26 @@ const ReviewEntry = ({ review }) => {
   const options = { year: 'numeric', month: 'short', day: 'numeric' };
   const formattedDate = dateObj.toLocaleDateString('en-US', options);
 
+  const [showFullReview, setShowFullReview] = useState(false);
+
+  const handleShowMore = () => {
+    setShowFullReview(true);
+  }
+
   return (
     <div className="relative box-border h-50 w-50 p-4 border-4 reviewTile" key={review.review_id}>
       <div className="absolute top-0 right-0 h-50 w-50 font-light">
         {review.reviewer_name},{formattedDate}
       </div>
       <Star rating={review.rating} />
+
+      <div className={`reviewBody font-normal ${showFullReview ? 'expanded' : ''}`}>
+        {showFullReview ? review.body : review.body.substring(0, 250)}
+        {!showFullReview && review.body.length > 250 && (
+          <span className="showMore" onClick={handleShowMore}>Show more</span>
+        )}
+      </div>
+
       <div className="reviewSummary font-semibold">{review.summary}</div>
       <div className="reviewBody font-normal">{review.body}</div>
       <div>{review.recommend ? 'I recommend this product' : null}</div>
