a3afa69056ba730a0d542a33ff799b9935ce9cc5
Tonegawa94 <victor1394@gmail.com>
2023-08-20 11:43:58 -0500

    added useState and handleSearch, no functionality

--- client/src/components/componentV/QnAcomponents/searchbar.jsx
+++ client/src/components/componentV/QnAcomponents/searchbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SearchBar = (props) => {
+
+const [search, updateSearch] = useState('')
+
+const handleSearch = () => {
+
+}
 
   return (
     <form className="mt-10 text text-black text-xl">
       <input type={'text'} placeholder={'Have a Question? search for asnwers...'} className="w-11/12 h-14" onChange={(e) => {
-
+        handleSearch(e)
       }}/>
     </form>
   )
