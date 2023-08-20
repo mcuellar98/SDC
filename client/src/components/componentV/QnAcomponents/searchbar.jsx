e6200aa07c928e7c3ac26e22f7922450951bb8fc
Tonegawa94 <victor1394@gmail.com>
2023-08-20 11:39:38 -0500

    changing branches

--- client/src/components/componentV/QnAcomponents/searchbar.jsx
+++ client/src/components/componentV/QnAcomponents/searchbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBar = () => {
 
   return (
-    <form className="mt-10">
+    <form className="mt-10 text text-black text-xl">
       <input type={'text'} placeholder={'Have a Question? search for asnwers...'} className="w-11/12 h-14"/>
     </form>
   )
