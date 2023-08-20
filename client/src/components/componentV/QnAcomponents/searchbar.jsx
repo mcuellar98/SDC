ad39519b60780a64c10d38defa301dad60c3923a
Tonegawa94 <victor1394@gmail.com>
2023-08-20 11:40:59 -0500

    added text size and color to search bar

--- client/src/components/componentV/QnAcomponents/searchbar.jsx
+++ client/src/components/componentV/QnAcomponents/searchbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
 
   return (
     <form className="mt-10 text text-black text-xl">
-      <input type={'text'} placeholder={'Have a Question? search for asnwers...'} className="w-11/12 h-14"/>
+      <input type={'text'} placeholder={'Have a Question? search for asnwers...'} className="w-11/12 h-14" onChange={(e) => {
+
+      }}/>
     </form>
   )
 }
